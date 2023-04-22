984f3ab3fdf2a5761f0b6d14d686ce14e1fa53aa
AndreyIvanov <hiaa@mail.ru>
2023-04-22 10:41:16 +0600

    Add files via upload

--- Statistics SBG.user.js
+++ Statistics SBG.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Statistics SBG
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://3d.sytes.net/
@@ -10,17 +10,40 @@
 // @updateURL    https://github.com/AndreyIvanov/hald/raw/main/Statistics%20SBG.user.js
 // @grant        none
 // ==/UserScript==
-/* globals jQuery, $, waitForKeyElements, ol */
+/* globals jQuery, $ */
 
 (function() {
     'use strict';
+    async function GetStatistic(username){
+        var now = new Date();
+        const self_data_req = $.ajax('/api/profile', {
+            method: 'get',data: { name: username },
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('auth')}`
+        },
+            success: function(self_data){
+                console.log(self_data);
+                const data_req = $.ajax('https://script.google.com/macros/s/AKfycbzj-Jd6HpHCIMbl10Gtf8sfjdaUR_LcQ46RC96Xj7Svlt2sKq3HesKK8hl3v-hoeeBm/exec',
+                                        {
+                    method: 'get',
+                    xhrFields: { withCredentials: true },
+                    crossDomain: true,
+                    dataType: "jsonp",
+                    contentType: "application/json",
+                    data: {dt:now, d: JSON.stringify(self_data.data)},
+                    success:function(rq){
+                        console.log('docs answer=',rq);
+                    }
 
-    let rButt = document.createElement('button');
-    rButt.innerText = 'Sendstat';
-    rButt.addEventListener('click', event => {
+                });
+            }
+        });
+
+    }
+    let rgButt = document.createElement('button');
+    rgButt.innerText = 'Stat G';
+    rgButt.addEventListener('click', event => {
         const pList = [
-            'Shkidlyak',
-            'aai79',
             'Antman55',
             'Mozg',
             'ilstransco',
@@ -28,35 +51,28 @@
             'VTEC55',
             'retty8',
             'D1MS',
-            'uriy2',
-            'XmPeakHigh',
             'LaasEnl',
+        ];
+        pList.forEach(e => {
+            GetStatistic(e);
+        });//endforeach
+
+    });
+    document.querySelector('.inventory__controls').appendChild(rgButt);
+    let rbButt = document.createElement('button');
+    rbButt.innerText = 'Stat B';
+    rbButt.addEventListener('click', event => {
+        const pList = [
+            'Shkidlyak',
+            'aai79',
+            'uriy2',
             'Lubom',
             'Crazy'
         ];
         pList.forEach(e => {
-            const self_data_req = $.ajax('/api/profile', {
-                method: 'get',data: { name: e },
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('auth')}`
-        },
-                success: function(self_data){
-                    console.log(self_data);
-                    var now = new Date();
-                    const data_req = $.ajax('https://script.google.com/macros/s/AKfycbyEh1_SaCUm4x_zPt5KMS6coyylAtcvs1QO8taI7OE/dev',
-                                            {
-                        method: 'get',
-                        xhrFields: { withCredentials: true },
-                        crossDomain: true,
-                        dataType: "jsonp",
-                        contentType: "application/json",
-                        data: {dt:now, d: JSON.stringify(self_data.data)},
-
-                    });
-                }
-            });
+            GetStatistic(e);
         });//endforeach
 
     });
-    document.querySelector('.inventory__controls').appendChild(rButt);
+    document.querySelector('.inventory__controls').appendChild(rbButt);
 })();
